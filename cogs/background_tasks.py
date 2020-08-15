41d2e784b236fd332202c331883f691215ff848a
Peter PARE <ppare@ns201027.ip-91-121-115.eu>
2020-08-15 16:40:38 +0200

    Editing roles works

--- cogs/background_tasks.py
+++ cogs/background_tasks.py
@@ -5,6 +5,8 @@ from discord.utils import get
 import time
 import os
 import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
 
 class backgroundTasks(commands.Cog):
 
@@ -18,6 +20,8 @@ class backgroundTasks(commands.Cog):
     self.options = {
       'usersync': self.userSync,
     }
+
+    self.guild_id = os.getenv('guild_id')
   
   @commands.Cog.listener()
   async def on_ready(self):
@@ -37,11 +41,68 @@ class backgroundTasks(commands.Cog):
     )
     c = conn.cursor()
 
+    query = "SELECT * FROM discord_data"
+    c.execute(query)
+    discord_data = c.fetchall()
     query = "SELECT * FROM users"
     c.execute(query)
-    result = c.fetchall()
-    for row in result:
-      print(f"User {row[2]} {row[3]}")
+    user_data = c.fetchall()
+
+    guild = self.client.get_guild(int(self.guild_id))
+    users = guild.members
+
+    atc_rank_roles = {
+      "2": get(guild.roles, id=int(os.getenv('r_s1'))),
+      "3": get(guild.roles, id=int(os.getenv('r_s2'))),
+      "4": get(guild.roles, id=int(os.getenv('r_s3'))),
+      "5": get(guild.roles, id=int(os.getenv('r_c1'))),
+      "7": get(guild.roles, id=int(os.getenv('r_c3'))),
+      "8": get(guild.roles, id=int(os.getenv('r_i1'))),
+      "10": get(guild.roles, id=int(os.getenv('r_i3'))),
+    }
+    rankS1 = get(guild.roles, id=int(os.getenv('r_s1')))
+    rankS2 = get(guild.roles, id=int(os.getenv('r_s2')))
+    rankS3 = get(guild.roles, id=int(os.getenv('r_s3')))
+    rankC1 = get(guild.roles, id=int(os.getenv('r_c1')))
+    rankC3 = get(guild.roles, id=int(os.getenv('r_c3')))
+    ranki1 = get(guild.roles, id=int(os.getenv('r_i1')))
+    ranki3 = get(guild.roles, id=int(os.getenv('r_i3')))
+
+    atc_role = get(guild.roles, id=int(os.getenv('r_atc')))
+    member_role = get(guild.roles, id=int(os.getenv('r_member')))
+    guest_role = get(guild.roles, id=int(os.getenv('r_guest')))
+    atc_mentor = get(guild.roles, id=int(os.getenv('r_mentoratc')))
+    pilote_mentor = get(guild.roles, id=int(os.getenv('r_mentorpilot')))
+
+    for d in discord_data:
+      for u in user_data:
+        if u[0] == d[1]:
+          print(f"Linked user found - Name: {u[2]} {u[3]}")
+          for us in users:
+            if us.id == int(d[2]):
+              print(f"User found: {us.id}")
+              username = f"{u[2]} - {us.id}"
+              user_atc_rank = u[8]
+              if user_atc_rank in atc_rank_roles:
+                if not atc_role in us.roles:
+                  await us.add_roles(atc_role)
+                
+                for ar in atc_rank_roles:
+                  if ar == user_atc_rank and not atc_rank_roles[ar] in us.roles:
+                    await us.add_roles(atc_rank_roles[ar])
+                  if ar != user_atc_rank and atc_rank_roles[ar] in us.roles:
+                    await us.remove_roles(atc_rank_roles[ar])
+              
+              else:
+                if atc_role in us.roles:
+                  await us.remove_roles(atc_role)
+                
+                for ar in atc_rank_roles:
+                  if ar == user_atc_rank and not atc_rank_roles[ar] in us.roles:
+                    await us.add_roles(atc_rank_roles[ar])
+                  if ar != user_atc_rank and atc_rank_roles[ar] in us.roles:
+                    await us.remove_roles(atc_rank_roles[ar])
+
     
     c.close()
 
